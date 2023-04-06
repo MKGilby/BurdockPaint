4461137494029a6c27761d02dceeee6ba3f20ec9
MKGilby <gilby@mksztsz.hu>
2023-04-06 17:39:37 +0200

    Button optimization
    - Button's texture is only updated when button state (selected or enabled) is changed. (Gaining 6-7 FPS)

--- source/BDPButtonUnit.pas
+++ source/BDPButtonUnit.pas
@@ -24,9 +24,14 @@ type
     fTLImage,fTRImage,fBLImage,fBRImage:TARGBImage;
     fFont2:TFont;
     fMessage:TMessage;
+    procedure RefreshImage;
+    procedure fSetSelected(value:boolean);
+    procedure fSetEnabled(value:boolean);
   published
     property Hint:string read fHint write fHint;
     property AssignedObject:TObject read fAssignedObject write fAssignedObject;
+    property Selected:boolean read fSelected write fSetSelected;
+    property Enabled:boolean read fEnabled write fSetEnabled;
   end;
 
 implementation
@@ -57,6 +62,7 @@ begin
   fBRImage:=MM.Images.ItemByName['ArchBottomRight'];
   fMessage:=iClickMessage;
   RecreateTexture;
+  RefreshImage;
 end;
 
 constructor TBDButton.Create;
@@ -71,29 +77,6 @@ end;
 procedure TBDButton.Draw;
 begin
   if Visible then begin
-    with fTexture.ARGBImage do begin
-      Bar(8,0,Width-16,3,OverlayImage.Palette[2]);
-      Bar(8,Height-3,fWidth-16,3,OverlayImage.Palette[2]);
-      Bar(0,8,3,Height-16,OverlayImage.Palette[2]);
-      Bar(Width-3,8,3,Height-16,OverlayImage.Palette[2]);
-      if fSelected then
-        Bar(3,3,Width-6,Height-6,OverlayImage.Palette[4])
-      else begin
-        if fEnabled then
-          Bar(3,3,Width-6,Height-6,OverlayImage.Palette[3])
-        else
-          Bar(3,3,Width-6,Height-6,OverlayImage.Palette[2]);
-      end;
-    end;
-    fTLImage.CopyTo(0,0,fTLImage.Width,fTLImage.Height,0,0,fTexture.ARGBImage,true);
-    fTRImage.CopyTo(0,0,fTRImage.Width,fTRImage.Height,fWidth-8,0,fTexture.ARGBImage,true);
-    fBLImage.CopyTo(0,0,fBLImage.Width,fBLImage.Height,0,fHeight-8,fTexture.ARGBImage,true);
-    fBRImage.CopyTo(0,0,fBRImage.Width,fBRImage.Height,fWidth-8,fHeight-8,fTexture.ARGBImage,true);
-    if not fSelected then
-      fFont.OutText(fTexture.ARGBImage,fCaption,fTextAlignPointX-fLeft,fTextAlignPointY+fTextOffsetY-fTop,fTextAlignX)
-    else
-      fFont2.OutText(fTexture.ARGBImage,fCaption,fTextAlignPointX-fLeft,fTextAlignPointY+fTextOffsetY-fTop,fTextAlignX);
-    fTexture.Update;
     PutTexture(fLeft,fTop,fTexture);
   end;
 end;
@@ -114,6 +97,49 @@ begin
     1:begin  // Left click
         if fMessage.TypeID<>MSG_NONE then MessageQueue.AddMessage(fMessage);
       end;
+  end;
+end;
+
+procedure TBDButton.RefreshImage;
+begin
+  with fTexture.ARGBImage do begin
+    Bar(8,0,Width-16,3,OverlayImage.Palette[2]);
+    Bar(8,Height-3,fWidth-16,3,OverlayImage.Palette[2]);
+    Bar(0,8,3,Height-16,OverlayImage.Palette[2]);
+    Bar(Width-3,8,3,Height-16,OverlayImage.Palette[2]);
+    if fSelected then
+      Bar(3,3,Width-6,Height-6,OverlayImage.Palette[4])
+    else begin
+      if fEnabled then
+        Bar(3,3,Width-6,Height-6,OverlayImage.Palette[3])
+      else
+        Bar(3,3,Width-6,Height-6,OverlayImage.Palette[2]);
+    end;
+  end;
+  fTLImage.CopyTo(0,0,fTLImage.Width,fTLImage.Height,0,0,fTexture.ARGBImage,true);
+  fTRImage.CopyTo(0,0,fTRImage.Width,fTRImage.Height,fWidth-8,0,fTexture.ARGBImage,true);
+  fBLImage.CopyTo(0,0,fBLImage.Width,fBLImage.Height,0,fHeight-8,fTexture.ARGBImage,true);
+  fBRImage.CopyTo(0,0,fBRImage.Width,fBRImage.Height,fWidth-8,fHeight-8,fTexture.ARGBImage,true);
+  if not fSelected then
+    fFont.OutText(fTexture.ARGBImage,fCaption,fTextAlignPointX-fLeft,fTextAlignPointY+fTextOffsetY-fTop,fTextAlignX)
+  else
+    fFont2.OutText(fTexture.ARGBImage,fCaption,fTextAlignPointX-fLeft,fTextAlignPointY+fTextOffsetY-fTop,fTextAlignX);
+  fTexture.Update;
+end;
+
+procedure TBDButton.fSetSelected(value:boolean);
+begin
+  if fSelected<>value then begin
+    fSelected:=value;
+    RefreshImage;
+  end;
+end;
+
+procedure TBDButton.fSetEnabled(value:boolean);
+begin
+  if fEnabled<>value then begin
+    fSelected:=value;
+    RefreshImage;
   end;
 end;
 
