f3bfcd2946a30bd21e886554cf3d69f0bd805424
HUNGilby <gilby@mksztsz.hu>
2023-04-27 15:51:44 +0200

    Update BDPModalDialogs.pas
    - You can specify shortcut keys in MessageBox. Place ^ before the letter you want to be the shortcut. For example:
    MessageBox('EXIT BURDOCK PAINT?','^YES;^NO');

--- source/BDPModalDialogs.pas
+++ source/BDPModalDialogs.pas
@@ -62,6 +62,7 @@ type
   TBDMessageBox=class(TBDModalDialog)
     constructor Create(iMessage:string;iButtons:string='OK');
     function Run:integer;
+    function KeyDown(Sender:TObject;key:integer):boolean;
   end;
 
 function MessageBox(iMessage:string;iButtons:string='OK'):integer;
@@ -397,7 +398,7 @@ end;
 { TBDMessageBox }
 
 constructor TBDMessageBox.Create(iMessage:string; iButtons:string);
-var atmB:TBDButton;i,x,buttoncount:integer;
+var atmB:TBDButton;i,x,buttoncount,key:integer;s:string;
 begin
   if length(iButtons)=0 then iButtons:='OK';
   if iButtons[length(iButtons)]<>';' then iButtons+=';';
@@ -413,13 +414,20 @@ begin
   x:=(fTexture.Width-(buttoncount*(NORMALBUTTONWIDTH+18))+18) div 2;
   i:=0;
   while length(iButtons)>0 do begin
+    key:=-1;
+    s:=copy(iButtons,1,pos(';',iButtons)-1);
+    if (pos('^',s)>0) and (pos('^',s)<length(s)) then begin
+      key:=SDL_SCANCODE_A+ord(s[pos('^',s)+1])-65;
+      delete(s,pos('^',s),1);
+    end;
     atmB:=TBDButton.Create(
       fWindowLeft+x,
       fWindowTop+MESSAGEBOXHEIGHT-44,
       NORMALBUTTONWIDTH,
-//      'OKX','CLOSE DIALOG',TMessage.Init(MSG_MESSAGEBOXRESP,i));
-      copy(iButtons,1,pos(';',iButtons)-1),'',TMessage.Init(MSG_MESSAGEBOXRESP,i));
-    atmb.ZIndex:=MODALDIALOG_ZINDEX+1;
+      s,'',TMessage.Init(MSG_MESSAGEBOXRESP,i));
+    atmB.ZIndex:=MODALDIALOG_ZINDEX+1;
+    atmB.Tag:=key;
+    atmB.OnKeyDown:=KeyDown;
     AddChild(atmB);
     delete(iButtons,1,pos(';',iButtons));
     inc(i);
@@ -431,8 +439,6 @@ end;
 function TBDMessageBox.Run:integer;
 var msg:TMessage;
 begin
-//  MouseObjects.List;
-  MouseObjects.ListVisible;
   Result:=-1;
   repeat
     SDL_SetRenderDrawColor(PrimaryWindow.Renderer,48,12,24,255);
@@ -447,6 +453,17 @@ begin
       end;
     end;
   until Result<>-1;
+end;
+
+function TBDMessageBox.KeyDown(Sender:TObject; key:integer):boolean;
+begin
+  Result:=false;
+  if (Sender is TBDButton) then begin
+    if key=TBDButton(Sender).Tag then begin
+      MessageQueue.AddMessage(TBDButton(Sender).Message);
+      Result:=true;
+    end;
+  end;
 end;
 
 end.
