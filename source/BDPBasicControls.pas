48fc1c1b003f43cecaca69fcb2a327cd08fc1c36
HUNGilby <gilby@mksztsz.hu>
2023-08-25 15:53:45 +0200

    Update BDPBasicControls.pas
    - Yuu can click the big colorful area to move the crosshair. (No values are changing yet.)

--- source/BDPBasicControls.pas
+++ source/BDPBasicControls.pas
@@ -96,11 +96,14 @@ type
   TBDHSBox=class(TVisibleControl)
     constructor Create(iLeft,iTop,iWidth,iHeight:integer);
     procedure SetColor(pH,pS:byte);
+    procedure Click(Sender:TObject;x,y,button:integer);
   protected
     procedure ReDraw; override;
   private
     fColor:uint32;
     fX,fY:integer;   // current crosshair center position
+    function GetColorByH(pX:integer):uint32;
+    function ModifyColorByS(pColor:uint32;pY:integer):uint32;
   public
     property Color:uint32 read fColor;
   end;
@@ -422,8 +425,10 @@ begin
   Height:=iHeight;
   fX:=0;
   fY:=0;
+  fColor:=$ffff0000;
   fVisible:=true;
   fNeedRedraw:=true;
+  OnClick:=Click;
 end;
 
 procedure TBDHSBox.SetColor(pH,pS:byte);
@@ -432,19 +437,21 @@ begin
   fY:=(pS*(fHeight-6) div 255);
 end;
 
+procedure TBDHSBox.Click(Sender:TObject; x,y,button:integer);
+begin
+  x-=fLeft;y-=fTop;
+  if x<3 then x:=3
+  else if x>fWidth-3-1 then x:=fWidth-3-1;
+  if y<3 then y:=3
+  else if y>fHeight-3-1 then y:=fHeight-3-1;
+  fX:=x-3;
+  fY:=y-3;
+  fColor:=ModifyColorByS(GetColorByH(fX+5),fY+5);
+  Refresh;
+end;
+
 procedure TBDHSBox.ReDraw;
-var i,j,w,h:integer;r,g,b:integer;
-
-  function lerp1(value,lo,hi:integer):integer;
-  begin
-    Result:=(value-lo)*255 div (hi-lo);
-  end;
-
-  function lerp2(value,max,start:integer):integer;
-  begin
-    Result:=start+value*(128-start) div max;
-  end;
-
+var i,j:integer;c:uint32;
 begin
   if Assigned(fTexture) then begin
     with fTexture.ARGBImage do begin
@@ -452,46 +459,10 @@ begin
       Bar(0,Height-3,fWidth,3,SystemPalette[2]);
       Bar(0,3,3,Height-6,SystemPalette[2]);
       Bar(Width-3,3,3,Height-6,SystemPalette[2]);
-      w:=fWidth-6;
-      h:=fHeight-6;
-      for i:=0 to w-1 do begin
-        if (i>=0) and (i<w div 6) then begin
-          r:=255;
-          g:=lerp1(i,0,w div 6-1);
-          b:=0;
-        end else
-        if (i>=w div 6) and (i<w div 3) then begin
-          r:=255-lerp1(i,w div 6,w div 3-1);
-          g:=255;
-          b:=0;
-        end else
-        if (i>=w div 3) and (i<w div 2) then begin
-          r:=0;
-          g:=255;
-          b:=lerp1(i,w div 3,w div 2-1);
-        end else
-        if (i>=w div 2) and (i<w*2 div 3) then begin
-          r:=0;
-          g:=255-lerp1(i,w div 2,w*2 div 3-1);
-          b:=255;
-        end else
-        if (i>=w*2 div 3) and (i<w*5 div 6) then begin
-          r:=lerp1(i,w*2 div 3,w*5 div 6-1);
-          g:=0;
-          b:=255;
-        end;
-        if (i>=w*5 div 6) and (i<w) then begin
-          r:=255;
-          g:=0;
-          b:=255-lerp1(i,w*5 div 6,w-1);
-        end;
-        for j:=0 to h-1 do begin
-          PutPixel(i+3,j+3,
-            lerp2(j,h-1,r),
-            lerp2(j,h-1,g),
-            lerp2(j,h-1,b),
-            255);
-        end;
+      for i:=3 to fWidth-3-1 do begin
+        c:=GetColorByH(i);
+        for j:=3 to fHeight-3-1 do
+          PutPixel(i,j,ModifyColorByS(c,j));
       end;
       Bar(3+fX+3,3+fY,4,2,0,0,0);
       Bar(3+fX,3+fY+3,2,4,0,0,0);
@@ -500,6 +471,71 @@ begin
     end;
     fTexture.Update;
   end;
+end;
+
+function TBDHSBox.GetColorByH(pX:integer):uint32;
+var w:integer;r,g,b:byte;
+
+  function lerp1(value,lo,hi:integer):integer;
+  begin
+    Result:=(value-lo)*255 div (hi-lo);
+  end;
+
+begin
+  w:=fWidth-6;
+  pX-=3;
+  if pX<0 then pX:=0
+  else if pX>w-1 then pX:=w-1;
+  if (pX>=0) and (pX<w div 6) then begin
+    r:=255;
+    g:=lerp1(pX,0,w div 6-1);
+    b:=0;
+  end else
+  if (pX>=w div 6) and (pX<w div 3) then begin
+    r:=255-lerp1(pX,w div 6,w div 3-1);
+    g:=255;
+    b:=0;
+  end else
+  if (pX>=w div 3) and (pX<w div 2) then begin
+    r:=0;
+    g:=255;
+    b:=lerp1(pX,w div 3,w div 2-1);
+  end else
+  if (pX>=w div 2) and (pX<w*2 div 3) then begin
+    r:=0;
+    g:=255-lerp1(pX,w div 2,w*2 div 3-1);
+    b:=255;
+  end else
+  if (pX>=w*2 div 3) and (pX<w*5 div 6) then begin
+    r:=lerp1(pX,w*2 div 3,w*5 div 6-1);
+    g:=0;
+    b:=255;
+  end;
+  if (pX>=w*5 div 6) and (pX<w) then begin
+    r:=255;
+    g:=0;
+    b:=255-lerp1(pX,w*5 div 6,w-1);
+  end;
+  Result:=$ff000000+(r<<16)+(g<<8)+b;
+end;
+
+function TBDHSBox.ModifyColorByS(pColor:uint32; pY:integer):uint32;
+var h:integer;r,g,b:byte;
+
+  function lerp2(value,max,start:integer):integer;
+  begin
+    Result:=start+value*(128-start) div max;
+  end;
+
+begin
+  h:=fHeight-6;
+  pY-=3;
+  if pY<0 then py:=0
+  else if pY>h-1 then py:=h-1;
+  r:=lerp2(pY,h-1,(pColor and $FF0000)>>16);
+  g:=lerp2(pY,h-1,(pColor and $FF00)>>8);
+  b:=lerp2(pY,h-1,pColor and $FF);
+  Result:=pColor and $FF000000+(r<<16)+(g<<8)+b;
 end;
 
 { TBDLightSlider }
