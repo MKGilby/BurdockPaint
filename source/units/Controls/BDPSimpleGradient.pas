a4deaf77b2459ec12f20133d3c43699ddc457c0f
MKGilby <gilby@mksztsz.hu>
2023-11-06 18:13:20 +0100

    Update BDPSimpleGradient.pas
    - You can set gradient after creating control,
    - You can set Selected property to highlight control.
    - If no gradient is specified when creating control, it won't draw itself. (It draws a full transparent box.)

--- source/units/Controls/BDPSimpleGradient.pas
+++ source/units/Controls/BDPSimpleGradient.pas
@@ -18,8 +18,12 @@ type
     fGradient:TGradient;
     fColorsWidth:integer;
     function fGetColorsWidth:integer;
+    procedure fSetGradient(value:TGradient);
+    procedure fSetSelected(value:boolean);
   public
     property ColorsWidth:integer read fGetColorsWidth;
+    property Gradient:TGradient write fSetGradient;
+    property Selected:boolean read fSelected write fSetSelected;
   end;
 
 
@@ -38,26 +42,32 @@ begin
   Width:=iWidth;
   Height:=iHeight;
   fNeedRedraw:=true;
+  Selected:=false;
 //  OnClick:=Click;
 end;
 
 procedure TBDSimpleGradient.ReDraw;
-var i:integer;
+var i:integer;c:uint32;
 begin
   if Assigned(fTexture) then begin
-    fColorsWidth:=Width-6;
-    with fTexture.ARGBImage do begin
-      // Outer border
-      Bar(0,0,Width,3,SystemPalette[SYSTEMCOLORDARK]);
-      Bar(0,Height-3,Width,3,SystemPalette[SYSTEMCOLORDARK]);
-      Bar(0,3,3,Height-6,SystemPalette[SYSTEMCOLORDARK]);
-      Bar(Width-3,3,3,Height-6,SystemPalette[SYSTEMCOLORDARK]);
-      // Gradient bar
-      if Assigned(fGradient) then begin
+    if Assigned(fGradient) then begin
+      fColorsWidth:=Width-6;
+      with fTexture.ARGBImage do begin
+        if not Selected then
+          c:=SystemPalette[SYSTEMCOLORDARK]
+        else
+          c:=SystemPalette[SYSTEMCOLORHIGHLIGHT];
+        // Outer border
+        Bar(0,0,Width,3,c);
+        Bar(0,Height-3,Width,3,c);
+        Bar(0,3,3,Height-6,c);
+        Bar(Width-3,3,3,Height-6,c);
+        // Gradient bar
         for i:=0 to fColorsWidth-1 do
           VLine(i+3,3,Height-6,fGradient.GetColorAt(i/(fColorsWidth-1)));
       end;
-    end;
+    end else
+      fTexture.ARGBImage.Bar(0,0,Width,Height,0);
     fTexture.Update;
   end;
 end;
@@ -65,6 +75,22 @@ end;
 function TBDSimpleGradient.fGetColorsWidth: integer;
 begin
   Result:=fColorsWidth+6;
+end;
+
+procedure TBDSimpleGradient.fSetGradient(value: TGradient);
+begin
+  if value<>fGradient then begin
+    fGradient:=value;
+    Refresh;
+  end;
+end;
+
+procedure TBDSimpleGradient.fSetSelected(value:boolean);
+begin
+  if (value<>fSelected) then begin
+    fSelected:=value;
+    Refresh;
+  end;
 end;
 
 end.
