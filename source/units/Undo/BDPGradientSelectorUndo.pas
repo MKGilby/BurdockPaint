221ecebcf89c2cc4456aac6ce9d24029b85e2df6
HUNGilby <gilby@mksztsz.hu>
2023-11-14 14:58:14 +0100

    Update BDPGradientSelectorUndo.pas
    - Added UndoItem types for Add/Delete gradient.

--- source/units/Undo/BDPGradientSelectorUndo.pas
+++ source/units/Undo/BDPGradientSelectorUndo.pas
@@ -29,9 +29,9 @@ uses
 
 type
 
-  { TBDGradientSelectorUndoItem }
+  { TBDGradientSelectorUndoItem_EDIT }
 
-  TBDGradientSelectorUndoItem=class(TBDUndoItem)
+  TBDGradientSelectorUndoItem_EDIT=class(TBDUndoItem)
     constructor Create(iGradientIndex:integer;iAffectedGradient,iNewGradient:TGradient);
     constructor CreateFromStream(iStream:TStream);
     destructor Destroy; override;
@@ -43,13 +43,42 @@ type
     fIndex:integer;
     fOldGradient,
     fNewGradient:TGradient;
-    procedure LoadFromStreamV1(pStream:TStream);
+  end;
+
+  { TBDGradientSelectorUndoItem_DELETE }
+
+  TBDGradientSelectorUndoItem_DELETE=class(TBDUndoItem)
+    constructor Create(iGradientIndex:integer);
+    constructor CreateFromStream(iStream:TStream);
+    destructor Destroy; override;
+    procedure Undo; override;
+    procedure Redo; override;
+    procedure SaveToStream(pStream:TStream); override;
+    procedure LoadFromStream(pStream:TStream); override;
+  private
+    fIndex:integer;
+    fOldGradient:TGradient;
+  end;
+
+  { TBDGradientSelectorUndoItem_ADD }
+
+  TBDGradientSelectorUndoItem_ADD=class(TBDUndoItem)
+    constructor Create(iGradientIndex:integer);
+    constructor CreateFromStream(iStream:TStream);
+    procedure Undo; override;
+    procedure Redo; override;
+    procedure SaveToStream(pStream:TStream); override;
+    procedure LoadFromStream(pStream:TStream); override;
+  private
+    fIndex:integer;
   end;
 
   { TBDGradientSelectorUndoSystem }
 
   TBDGradientSelectorUndoSystem=class(TBDUndoSystem)
-    procedure AddUndo(pGradientIndex:integer;pAffectedGradient,pNewGradient:TGradient);
+    procedure AddUndo_EDIT(pGradientIndex:integer;pAffectedGradient,pNewGradient:TGradient);
+    procedure AddUndo_DELETE(pGradientIndex:integer);
+    procedure AddUndo_ADD(pGradientIndex:integer);
     procedure SaveToStream(pStream:TStream); override;
     procedure LoadFromStream(pStream:TStream); override;
   private
@@ -59,15 +88,15 @@ type
 
 implementation
 
-uses BDPShared;
+uses BDPShared, BDPInternalFileFormat;
 
 const
   UNDOOPERATIONGRADIENTSELECTORID='UDG';
   UNDOSYSTEMGRADIENTSELECTORID='USG';
 
-{ TBDGradientSelectorUndoItem }
+{ TBDGradientSelectorUndoItem_EDIT }
 
-constructor TBDGradientSelectorUndoItem.Create(iGradientIndex:integer;
+constructor TBDGradientSelectorUndoItem_EDIT.Create(iGradientIndex:integer;
   iAffectedGradient,iNewGradient:TGradient);
 begin
   fIndex:=iGradientIndex;
@@ -78,131 +107,219 @@ begin
   fRedoable:=true;
 end;
 
-constructor TBDGradientSelectorUndoItem.CreateFromStream(iStream:TStream);
+constructor TBDGradientSelectorUndoItem_EDIT.CreateFromStream(iStream:TStream);
 begin
   LoadFromStream(iStream);
 end;
 
-destructor TBDGradientSelectorUndoItem.Destroy;
+destructor TBDGradientSelectorUndoItem_EDIT.Destroy;
 begin
   if Assigned(fNewGradient) then fNewGradient.Free;
   if Assigned(fOldGradient) then fOldGradient.Free;
   inherited Destroy;
 end;
 
-procedure TBDGradientSelectorUndoItem.Undo;
+procedure TBDGradientSelectorUndoItem_EDIT.Undo;
 begin
   if (fIndex>=0) and (fIndex<Project.CurrentGradientList.Count) then
     Project.CurrentGradientList[fIndex].CopyFrom(fOldGradient);
 end;
 
-procedure TBDGradientSelectorUndoItem.Redo;
+procedure TBDGradientSelectorUndoItem_EDIT.Redo;
 begin
   if (fIndex>=0) and (fIndex<Project.CurrentGradientList.Count) then
     Project.CurrentGradientList[fIndex].CopyFrom(fNewGradient);
 end;
 
-procedure TBDGradientSelectorUndoItem.SaveToStream(pStream:TStream);
-var i:integer;curr:int64;s:string;
+procedure TBDGradientSelectorUndoItem_EDIT.SaveToStream(pStream:TStream);
+var Xs:TStream;
 begin
-  s:=UNDOOPERATIONGRADIENTSELECTORID+#1;
-  pStream.Write(s[1],4);
-  curr:=pStream.Position;
-  i:=0;
-  pStream.Write(i,4);  // Size placeholder
-
-  pStream.Write(fIndex,4);
-  fOldGradient.SaveToStream(pStream);
-  fNewGradient.SaveToStream(pStream);
-  i:=pStream.Position-curr-4;
-  pStream.Position:=curr;
-  pStream.write(i,4);
-  pStream.Position:=pStream.Position+i;
+  Xs:=TMemoryStream.Create;
+  try
+    Xs.Write(fIndex,4);
+    fOldGradient.SaveToStream(Xs);
+    fNewGradient.SaveToStream(Xs);
+    TInternalFileFormat.WriteBlock(pStream,UNDOOPERATIONGRADIENTSELECTORID,1,Xs,false);
+  finally
+    Xs.Free;
+  end;
 end;
 
-procedure TBDGradientSelectorUndoItem.LoadFromStream(pStream:TStream);
-var size,curr:int64;b:byte;s:string;
-begin
-  s:=#0#0#0#0;
-  pStream.Read(s[1],4);
-  if uppercase(copy(s,1,3))<>UNDOOPERATIONGRADIENTSELECTORID then
-    raise Exception.Create(Format('GradientSelector undo operation block id expected, got %s.',[copy(s,1,3)]));
-  if ord(s[1]) and $20<>0 then Exception.Create('GradientSelector undo operation block cannot be compressed!');
-  size:=0;
-  pStream.Read(Size,4);
-  curr:=pStream.Position;
-  b:=ord(s[4]);
-  if b=1 then LoadFromStreamV1(pStream)
-  else raise Exception.Create(Format('Unknown GradientSelector undo operation block version! (%d)',[b]));
-  fRedoable:=true;
-  pStream.Position:=curr+size;
-end;
-
-procedure TBDGradientSelectorUndoItem.LoadFromStreamV1(pStream:TStream);
+procedure TBDGradientSelectorUndoItem_EDIT.LoadFromStream(pStream:TStream);
 begin
   pStream.Read(fIndex,4);
   fOldGradient:=TGradient.CreateFromStream(pStream);
   fNewGradient:=TGradient.CreateFromStream(pStream);
+  fRedoable:=true;
 end;
+
+
+{ TBDGradientSelectorUndoItem_DELETE }
+
+constructor TBDGradientSelectorUndoItem_DELETE.Create(iGradientIndex:integer);
+begin
+  fIndex:=iGradientIndex;
+  fOldGradient:=TGradient.Create(0,0);
+  fOldGradient.CopyFrom(Project.CurrentGradientList[fIndex]);
+  fRedoable:=true;
+end;
+
+constructor TBDGradientSelectorUndoItem_DELETE.CreateFromStream(iStream:TStream);
+begin
+  LoadFromStream(iStream);
+end;
+
+destructor TBDGradientSelectorUndoItem_DELETE.Destroy;
+begin
+  if Assigned(fOldGradient) then fOldGradient.Free;
+  inherited Destroy;
+end;
+
+procedure TBDGradientSelectorUndoItem_DELETE.Undo;
+var tmp:TGradient;
+begin
+  tmp:=TGradient.Create(0,0);
+  tmp.CopyFrom(fOldGradient);
+  Project.CurrentGradientList.Insert(fIndex,tmp);
+end;
+
+procedure TBDGradientSelectorUndoItem_DELETE.Redo;
+begin
+  Project.CurrentGradientList.Delete(fIndex);
+end;
+
+procedure TBDGradientSelectorUndoItem_DELETE.SaveToStream(pStream:TStream);
+var Xs:TStream;
+begin
+  Xs:=TMemoryStream.Create;
+  try
+    Xs.Write(fIndex,4);
+    fOldGradient.SaveToStream(Xs);
+    TInternalFileFormat.WriteBlock(pStream,UNDOOPERATIONGRADIENTSELECTORID,2,Xs,false);
+  finally
+    Xs.Free;
+  end;
+end;
+
+procedure TBDGradientSelectorUndoItem_DELETE.LoadFromStream(pStream:TStream);
+begin
+  pStream.Read(fIndex,4);
+  fOldGradient:=TGradient.CreateFromStream(pStream);
+  fRedoable:=true;
+end;
+
+{ TBDGradientSelectorUndoItem_ADD }
+
+constructor TBDGradientSelectorUndoItem_ADD.Create(iGradientIndex:integer);
+begin
+  fIndex:=iGradientIndex;
+  fRedoable:=true;
+end;
+
+constructor TBDGradientSelectorUndoItem_ADD.CreateFromStream(iStream:TStream);
+begin
+  LoadFromStream(iStream);
+end;
+
+procedure TBDGradientSelectorUndoItem_ADD.Undo;
+begin
+  Project.CurrentGradientList.Delete(fIndex);
+end;
+
+procedure TBDGradientSelectorUndoItem_ADD.Redo;
+begin
+  Project.CurrentGradientList.Insert(fIndex,TGradient.Create($ff000000,$ffffffff));
+end;
+
+procedure TBDGradientSelectorUndoItem_ADD.SaveToStream(pStream:TStream);
+var Xs:TStream;
+begin
+  Xs:=TMemoryStream.Create;
+  try
+    Xs.Write(fIndex,4);
+    TInternalFileFormat.WriteBlock(pStream,UNDOOPERATIONGRADIENTSELECTORID,3,Xs,false);
+  finally
+    Xs.Free;
+  end;
+end;
+
+procedure TBDGradientSelectorUndoItem_ADD.LoadFromStream(pStream:TStream);
+begin
+  pStream.Read(pStream,4);
+  fRedoable:=true;
+end;
+
 
 { TBDGradientSelectorUndoSystem }
 
-procedure TBDGradientSelectorUndoSystem.AddUndo(pGradientIndex:integer;
+procedure TBDGradientSelectorUndoSystem.AddUndo_EDIT(pGradientIndex:integer;
   pAffectedGradient,pNewGradient:TGradient);
 begin
-  AddItem(TBDGradientSelectorUndoItem.Create(pGradientIndex,pAffectedGradient,pNewGradient));
+  AddItem(TBDGradientSelectorUndoItem_EDIT.Create(pGradientIndex,pAffectedGradient,pNewGradient));
+end;
+
+procedure TBDGradientSelectorUndoSystem.AddUndo_DELETE(pGradientIndex:integer);
+begin
+  AddItem(TBDGradientSelectorUndoItem_DELETE.Create(pGradientIndex));
+end;
+
+procedure TBDGradientSelectorUndoSystem.AddUndo_ADD(pGradientIndex:integer);
+begin
+  AddItem(TBDGradientSelectorUndoItem_ADD.Create(pGradientIndex));
 end;
 
 procedure TBDGradientSelectorUndoSystem.SaveToStream(pStream:TStream);
-var i:integer;curr:int64;s:string;
+var i:integer;Xs:TStream;
 begin
-  s:=UNDOSYSTEMGRADIENTSELECTORID+#1;
-  pStream.Write(s[1],4);
-  curr:=pStream.Position;
-  i:=0;
-  pStream.Write(i,4);  // Size placeholder
-
-  i:=Self.Count;
-  pStream.Write(i,2);
-  pStream.Write(fPointer,2);
-  for i:=0 to Self.Count-1 do
-    Self[i].SaveToStream(pStream);
-  i:=pStream.Position-curr-4;
-  pStream.Position:=curr;
-  pStream.write(i,4);
-  pStream.Position:=pStream.Position+i;
+  Xs:=TMemoryStream.Create;
+  try
+    i:=Self.Count;
+    Xs.Write(i,2);
+    Xs.Write(fPointer,2);
+    for i:=0 to Self.Count-1 do
+      Self[i].SaveToStream(Xs);
+    TInternalFileFormat.WriteBlock(pStream,UNDOSYSTEMGRADIENTSELECTORID,1,Xs);
+  finally
+    Xs.Free;
+  end;
 end;
 
 procedure TBDGradientSelectorUndoSystem.LoadFromStream(pStream:TStream);
-var size,curr:int64;b:byte;s:string;
+var tmp:TInternalBlock;
 begin
-  s:=#0#0#0#0;
-  pStream.Read(s[1],4);
-  if uppercase(copy(s,1,3))<>UNDOSYSTEMGRADIENTSELECTORID then
-    raise Exception.Create(Format('GradientSelector undosystem block id expected, got %s.',[copy(s,1,3)]));
-  if ord(s[1]) and $20<>0 then Exception.Create('GradientSelector undosystem block cannot be compressed!');
-
-  size:=0;
-  pStream.Read(Size,4);
-  curr:=pStream.Position;
-  b:=ord(s[4]);
-  case b of
-    1:LoadFromStreamV1(pStream);
-    else raise Exception.Create(Format('Unknown GradientSelector undosystem block version! (%d)',[b]));
+  tmp:=TInternalFileFormat.ReadBlock(pStream);
+  try
+    if tmp.BlockID<>UNDOSYSTEMGRADIENTSELECTORID then
+      raise Exception.Create(Format('GradientSelector undosystem block expected, got %s.',[tmp.BlockID]));
+    if tmp.Version=1 then LoadFromStreamV1(tmp.Data)
+    else raise Exception.Create(Format('Unknown GradientSelector undosystem block version! (%d)',[tmp.Version]));
+  finally
+    tmp.Free;
   end;
-  pStream.Position:=curr+size;
 end;
 
 procedure TBDGradientSelectorUndoSystem.LoadFromStreamV1(pStream:TStream);
-var count,i:integer;
+var count:integer;tmp:TInternalBlock;
 begin
   count:=0;
   pStream.Read(count,2);
   fPointer:=0;
   pStream.Read(fPointer,2);
   if fPointer=65535 then fPointer:=-1;
-  for i:=0 to Count-1 do
-    Self.Add(TBDGradientSelectorUndoItem.CreateFromStream(pStream));
+  while count>0 do begin
+    tmp:=TInternalFileFormat.ReadBlock(pStream);
+    try
+      if tmp.BlockID<>UNDOOPERATIONGRADIENTSELECTORID then
+        raise Exception.Create(Format('GradientSelector undo operation block expected, got %s.',[tmp.BlockID]));
+      if tmp.Version=1 then Self.Add(TBDGradientSelectorUndoItem_EDIT.CreateFromStream(tmp.Data))
+      else if tmp.Version=2 then Self.Add(TBDGradientSelectorUndoItem_DELETE.CreateFromStream(tmp.Data))
+      else if tmp.Version=3 then Self.Add(TBDGradientSelectorUndoItem_ADD.CreateFromStream(tmp.Data))
+      else raise Exception.Create(Format('Unknown GradientSelector undo operation block version! (%d)',[tmp.Version]));
+    finally
+      tmp.Free;
+    end;
+    dec(count);
+  end;
 end;
 
 end.
