4f05ac9a57ce54654cde1a84ac50772c6b321023
HUNGilby <gilby@mksztsz.hu>
2023-11-15 13:56:43 +0100

    Default palette
    - Changed format back to .COL

--- work/ntsccol/convert.bat
+++ work/ntsccol/convert.bat
@@ -1,2 +1,2 @@
 copy ..\base_copyright_notice.txt ..\..\source\includes\ntsccol.inc
-bin2inc ntsc.bdp ..\..\source\includes\ntsccol.inc NTSCCOL /a
+bin2inc ntsc.col ..\..\source\includes\ntsccol.inc NTSCCOL /a
