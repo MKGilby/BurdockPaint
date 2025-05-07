6094eef00a102c977888f12c73bd059e77b6ccdd
HUNGilby <gilby@mksztsz.hu>
2025-05-07 19:32:40 +0200

    Update build scripts

--- source/BuildRelease_x86.bat
+++ source/BuildRelease_x86.bat
@@ -1,7 +1,7 @@
 @echo off
-if not exist lib\ (mkdir lib)
-if not exist lib\x86_64-win64\ (mkdir lib\x86_64-win64) else (del /Q lib\x86_64-win64\*)
-C:\lazarus\fpc\3.2.2\bin\x86_64-win64\fpc.exe -l- -Twin32 -Pi386 -MDelphi -Scghi -CX -O3 -XX -v0 -vm6058,5024 -Filib\i386-win32 -Fuunits -Fuunits\* -FuC:\lazarus\lcl\units\i386-win32\win32 -FuC:\lazarus\lcl\units\i386-win32 -FuC:\lazarus\components\freetype\lib\i386-win32 -FuC:\lazarus\components\lazutils\lib\i386-win32 -FuC:\lazarus\packager\units\i386-win32 -Fu. -FUlib\x86_64-win64 -FE..\release\x86 -oBurdockPaint.exe -dLCL -dLCLwin32 BurdockPaint.lpr
-rem C:\lazarus\fpc\3.2.2\bin\x86_64-win64\fpc.exe -Twin32 -Pi386 -MDelphi -Scghi -CX -O3 -XX -l -vewi -vm6058,5024 -Filib\i386-win32 -Fuunits -Fuunits\* -FuC:\lazarus\lcl\units\i386-win32\win32 -FuC:\lazarus\lcl\units\i386-win32 -FuC:\lazarus\components\freetype\lib\i386-win32 -FuC:\lazarus\components\lazutils\lib\i386-win32 -FuC:\lazarus\packager\units\i386-win32 -Fu. -FUlib\x86_64-win64 -FE..\_release_x86 -oBurdockPaint.exe -dLCL -dLCLwin32 BurdockPaint.lpr
+call ..\work\setenv.bat
+if not exist lib\ (mkdir lib) else (del /Q lib\*)
+%FPCDIR%\fpc.exe -l- -Twin32 -Pi386 -MDelphi -Scghi -CX -O3 -XX -v0 -vm6058,5024 -Filib -Fuunits -Fuunits\* -FuC:\lazarus\lcl\units\i386-win32\win32 -FuC:\lazarus\lcl\units\i386-win32 -FuC:\lazarus\components\freetype\lib\i386-win32 -FuC:\lazarus\components\lazutils\lib\i386-win32 -FuC:\lazarus\packager\units\i386-win32 -Fu. -FUlib -FE..\release\x86 -oBurdockPaint.exe -dLCL -dLCLwin32 BurdockPaint.lpr
+rem %FPCDIR%\fpc.exe -l- -Twin32 -Pi386 -MDelphi -Scghi -CX -O3 -XX -vewi -vm6058,5024 -Filib -Fuunits -Fuunits\* -FuC:\lazarus\lcl\units\i386-win32\win32 -FuC:\lazarus\lcl\units\i386-win32 -FuC:\lazarus\components\freetype\lib\i386-win32 -FuC:\lazarus\components\lazutils\lib\i386-win32 -FuC:\lazarus\packager\units\i386-win32 -Fu. -FUlib -FE..\_release_x86 -oBurdockPaint.exe -dLCL -dLCLwin32 BurdockPaint.lpr
 RMDIR "lib" /S /Q
 
